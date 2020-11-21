73d7b0c79666515fecc93c049e0e2a183101279f
unknown <andjelapaunovic99@gmail.com>
2020-11-21 21:52:24 +0100

    Initial commit

---
+++ isa/src/main/java/pharmacy/isa/PharmacyProjectIsaApplication.java
@@ -0,0 +1,13 @@
+package pharmacy.isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyProjectIsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyProjectIsaApplication.class, args);
+	}
+
+}
---
+++ isa/src/test/java/pharmacy/isa/PharmacyProjectIsaApplicationTests.java
@@ -0,0 +1,13 @@
+package pharmacy.isa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyProjectIsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
