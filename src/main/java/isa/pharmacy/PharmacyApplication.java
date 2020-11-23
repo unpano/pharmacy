e711966be731b0dd7a10a0541779a22ce5c02dd4
Anđela Paunović <andjelapaunovic99@gmail.com>
2020-11-23 15:00:19 +0100

    Solved app run

--- src/main/java/isa/pharmacy/PharmacyApplication.java
+++ src/main/java/isa/pharmacy/PharmacyApplication.java
@@ -2,8 +2,9 @@ package isa.pharmacy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PharmacyApplication {
 
 	public static void main(String[] args) {
