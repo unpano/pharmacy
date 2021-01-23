14f8e0c2666c96648b99225bb86451a43576d130
Anđela Paunović <andjelapaunovic99@gmail.com>
2021-01-23 16:37:24 +0100

    Collect data from sign in/up forms

--- pharmacy/src/main/java/isa/pharmacy/Controllers/AnonymousUserController.java
+++ pharmacy/src/main/java/isa/pharmacy/Controllers/AnonymousUserController.java
@@ -1,6 +1,8 @@
 package isa.pharmacy.Controllers;
 
+import isa.pharmacy.Models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -11,13 +13,37 @@ public class AnonymousUserController {
         return "index";
     }
 
-    @RequestMapping("/login")
-    public String login() {
-        return "Login";
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("user", new User());
+
+        return "Login";}
+
+    @PostMapping("/createUser")
+    public String createUser(@ModelAttribute User user) {
+
+        //Ovde treba da se kreira novi User i da se sacuva u bazi
+        //Podaci su pokupljeni iz html-a i prosledjeni preko user-a ovoj metodi
+        //Zatim se redirektuje na index pocetnu stranicu
+
+        return "redirect:/";
     }
 
-    @RequestMapping("/signup")
-    public String signup() {
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+
         return "index";
+    }
+
+    @PostMapping("/findUser")
+    public String findUser(@ModelAttribute User user) {
+
+        //Ovde treba proveriti da li email i password odgovaraju nekom od vec
+        //registrovanih korisnika u bazi
+        //Podaci su pokupljeni iz html-a i prosledjeni preko user-a ovoj metodi
+        //Zatim se redirektuje na index pocetnu stranicu
+
+        return "redirect:/";
     }
 }
---
+++ pharmacy/src/main/java/isa/pharmacy/Models/User.java
@@ -0,0 +1,79 @@
+package isa.pharmacy.Models;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String address;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    private String city;
+    private String country;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
