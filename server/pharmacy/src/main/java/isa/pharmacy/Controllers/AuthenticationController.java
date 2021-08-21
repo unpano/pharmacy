9a6d697e4e0b5cb1f4da459d81a7e302bc30be1b
Hristina98 <hristisuknjaja@gmail.com>
2021-08-21 13:41:20 +0200

    test i
    test

--- server/pharmacy/src/main/java/isa/pharmacy/Controllers/AuthenticationController.java
+++ server/pharmacy/src/main/java/isa/pharmacy/Controllers/AuthenticationController.java
@@ -30,6 +30,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 //Kontroler zaduzen za autentifikaciju korisnika
+
+//hristina test
 @RestController
 @RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AuthenticationController {
