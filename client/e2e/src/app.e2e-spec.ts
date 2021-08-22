7c8bd8bb2c8fd688e179626b19369490d6f53e6a
Anđela Paunović <andjelapaunovic99@gmail.com>
2021-08-22 14:38:30 +0200

    Changed project configuration files

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('carInsuranceAngular app is running!');
+    expect(await page.getTitleText()).toEqual('pharmacyAngular app is running!');
   });
 
   afterEach(async () => {
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'carInsuranceAngular'`, () => {
+  it(`should have as title 'pharmacyAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('carInsuranceAngular');
+    expect(app.title).toEqual('pharmacyAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('carInsuranceAngular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('pharmacyAngular app is running!');
   });
 });
